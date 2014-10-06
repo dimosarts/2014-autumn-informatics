89cc39bd113e071b04104d7a27799420de821f01
Johnny-Kay <overlord.delta@gmail.com>
2014-10-06 14:39:41 +0600

    ABC

---
+++ Козин Иван/Вариант 3 задача 2.pas
@@ -0,0 +1,22 @@
+program var3num2;
+var
+  n,i,cr1,cr2,num:integer;
+begin
+write('Введите количество элементов последовательности: ');
+readln(n);
+writeln('Введите ',n,' членов последовательности: ');
+for i:=1 to n do
+  begin
+  readln(num);
+  cr1:=num mod 3;
+  if (cr1=0) then
+    begin
+    cr2:=num div 3;
+    if (cr2 mod 2<>0) then writeln('Число удовлетворяет условиям')
+    else writeln('Число при делении на 3 дает четное число');
+    end
+  else writeln('Число не делится на 3');
+  end;
+write('Программа завершена, для выхода нажмите Enter...');
+readln;
+end.
