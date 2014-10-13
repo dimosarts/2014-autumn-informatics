6fd0ff82e60d68d7e7f6017c84a151ab128b5823
Starling2804 <mos575@mail.ru>
2014-10-13 10:41:18 +0400

    Closed #87

--- Юрин Александр/6 билет №1.pas
+++ Юрин Александр/6 билет №1.pas
@@ -1,10 +1,10 @@
-var x,y,u:real;
+﻿var x,y,u:real;
 begin
  writeln('введите х ');
  read(x);
  writeln('введите у ');
  read(y);
- if (x*x+y*y<0.25) and (x>0) and (y>0) or (y<0) then
+ if (x*x+y*y<0.25 ) and (x>0) and (y>0) or (y<0) then
   u:=y*y+2*x*y
  else
   u:=x*x+2*x*y;
