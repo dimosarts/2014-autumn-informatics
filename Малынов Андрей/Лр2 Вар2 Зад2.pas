6a4edb0627591a292e58313b7fac4efccda5a1ed
Андрей Малынов <ls.17@yandex.ru>
2014-10-27 12:39:07 +0400

    Конец света близок

--- Малынов Андрей/Лр2 Вар2 Зад2.pas
+++ Малынов Андрей/Лр2 Вар2 Зад2.pas
@@ -1,19 +1,29 @@
-const n=10;
-var a:array [1..n]of integer;
-i,s:integer;
+const n=3;
+var a:array [1..n,1..n]of integer;
+i,j,min,max:integer;
 begin
- writeln('¬ведите массив из n чисел' );
- for i:=1 to n do 
-  read(a[i]);
- for i:=1 to ((n div 2) +1) do 
-  if a[i]=a[n-i+1] then  s:=1
-  else 
-   begin
-   s:=0;
-   break;
-   end;
- if (s=1) then
-  writeln('явл€етс€ палиндромом ') 
- else 
-  writeln('Ќе €вл€етс€ полиндромом ');
-end.  
+randomize;
+writeln('элементы матрицы ');
+for i:=1 to n do begin
+for j:=1 to n do begin 
+a[i,j]:=random(100);
+end;
+end; 
+for i:=1 to n do begin
+for j:=1 to n do
+write (a[i,j], ' ');  
+writeln;
+end;
+ min:=a[1,1];
+ max:=a[1,1];
+for i:=1 to n do 
+ begin
+ min:=a[i,1];
+ for j:=1 to n do 
+  begin
+  if (a[i,j]<min) then min:=a[i,j] 
+  end; 
+  if min>max then  max:=min; 
+ end;
+  writeln('максимальный элемент из минимальных элементов строки',' ',max);  
+  end.
