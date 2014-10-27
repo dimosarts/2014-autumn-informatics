a7fd9305059c4be826176ac59c644d2a06ec48cd
Андрей Малынов <ls.17@yandex.ru>
2014-10-27 12:10:18 +0400

    111

---
+++ Малынов Андрей/Лр2 Вар2 Зад2.pas
@@ -0,0 +1,19 @@
+const n=10;
+var a:array [1..n]of integer;
+i,s:integer;
+begin
+ writeln('¬ведите массив из n чисел' );
+ for i:=1 to n do 
+  read(a[i]);
+ for i:=1 to ((n div 2) +1) do 
+  if a[i]=a[n-i+1] then  s:=1
+  else 
+   begin
+   s:=0;
+   break;
+   end;
+ if (s=1) then
+  writeln('явл€етс€ палиндромом ') 
+ else 
+  writeln('Ќе €вл€етс€ полиндромом ');
+end.  
