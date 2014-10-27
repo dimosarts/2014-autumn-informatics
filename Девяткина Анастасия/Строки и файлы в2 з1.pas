957732517379fc14242f1da00738dd712875b6ab
Nastya9 <nasty-devyatkina@mail.com>
2014-10-28 00:28:53 +0300

    add files

---
+++ Девяткина Анастасия/Строки и файлы в2 з1.pas
@@ -0,0 +1,24 @@
+var
+  i, n,j: integer;
+   s0,s1 : string;
+
+begin
+  writeln('Введите строку');
+  readln(s0);
+  n := Length(s0);
+  writeln ('Длина исходной строки равна ',n);
+  s1:=' ';
+  for i:=1 to n do begin
+  if s0[i]<>'*' then 
+  s1:=s1+s0[i]+s0[i];
+  end;
+  if (s1=' ') then writeln('Полученная строка не содержит элементов')
+  else begin
+  writeln ('Полученная строка имеет вид', s1);
+   j:=length (s1);
+  writeln ('Длина полученной строки ', j);
+  end;
+  end.
+  
+  
+ 
