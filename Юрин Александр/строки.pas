39dc6fa4d11d7623f506daec713e4ceda9144db4
White2Dark <mas.dim10@mail.ru>
2014-10-27 13:00:38 +0400

    Closed #113

--- Юрин Александр/строки.pas
+++ Юрин Александр/строки.pas
@@ -3,10 +3,11 @@ s:string ;
 k:integer;
 begin 
 k:=1;  
- read(s);
- while k>0 do begin
- k:=pos('..',s);
- delete (s,k,1);
+ read(s); 
+ while k>0 do
+ begin
+  k:=pos('..',s);
+  delete (s,k,1);
  end;
   writeln(s);
   end.
