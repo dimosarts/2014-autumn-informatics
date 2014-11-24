7af7f5ac45d73df1d93a298f766e818f270d039c
lisaveta-z <lisaveta.z@rambler.ru>
2014-11-24 12:49:58 +0400

    unfinished

--- Маслов Дмитрий/ProgramTfile.pas
+++ Маслов Дмитрий/ProgramTfile.pas
@@ -1,27 +1,41 @@
 program Tfile;
 
+type
+  pupil = record
+    Name: string[8];
+    Surname: string[7];
+    Form: string[3];
+    FormWord: string[2];
+  end;
 
 var
-  f, g: text;
-  i,  j, k, m: integer;
-  s, s5, s2, s3, s4: string;
+  
+  p: pupil;
+  t: file of pupil;
+  i, n: integer;
+
+
 
 begin
-  j := 0;
-  k := 0;
-  
-  s4 := '-';
-  assign(f, 'C:\k01-221\2014-autumn-informatics\Маслов Дмитрий\FileF.txt');
-  assign(g, 'C:\k01-221\2014-autumn-informatics\Маслов Дмитрий\FileG.txt');
-  reset(f);
-  rewrite(g);
-  writeln('Исходный файл выглядит таким образом:');
-  while not eof(f) do
-  begin
-    readln(f, s);
-    writeln(s);
-    k := k + 1;
-  end;
-  reset(f);
- 
+  assign(t, 'class.db');
+  rewrite(t);
+  Writeln('Введите количество учеников');
+  readln(n);
+  for i := 1 to n do 
+    with p do 
+    begin
+      begin
+        write(i, '-ая фамилия:');
+        read(surname);
+        write('Имя:');
+        read(name);
+        write('Класс:');
+        read(Form);
+        write('Буква класса:');
+        read(FormWord);
+      end;
+      write(t, p);
+    end;
+  Writeln('Создание списка окончено');
+  close(t);
 end.
