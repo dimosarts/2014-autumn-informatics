1a18f1f171c8879659165c3c7a9850e0da4fc701
Vya4ik <stepanyanslava@mail.ru>
2014-12-08 11:17:47 +0400

    1

---
+++ Степанян Вячеслав/багаж2.pas
@@ -0,0 +1,28 @@
+TYPE
+ jir = record
+  fio: string[20];
+  kolvo: integer;
+  ves: real;
+end;
+VAR
+ spisok: jir;
+ sp: file of jir;
+ k:boolean;
+begin
+ k:=false;
+ assign (sp,'C:\mephi\passajir.txt');
+ reset (sp);
+ while not eof (sp) do
+  begin
+   read (sp, spisok);
+    with sp do begin
+     if (spisok.kolvo=1) and (spisok.ves>=30) then
+      k:=true;
+    end; 
+  end;
+ close (sp);
+ if k=true then
+  writeln ('такой пассажир есть')
+ else
+  writeln ('такого пассажира нет');
+end.
