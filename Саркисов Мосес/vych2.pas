1bca90b343d3138a222ebe64ff6cd070c960a403
Starling2804 <mos575@mail.ru>
2014-12-08 10:55:25 +0400

    123

--- Саркисов Мосес/vych2.pas
+++ Саркисов Мосес/vych2.pas
@@ -3,16 +3,10 @@ var i,x,res:  real;
 begin
     read(x);
     i:=1; res:=1;
-    while i<=63 do
+    while i<=32 do
       begin
-      res:=res/(x-i);
+      res:=res/(x-i)*(x-i-1);
       i:=i+2;
-      end;    
-    i:=2;
-    while i<=64 do
-      begin
-      res:=res*(x-i);
-      i:=i+2;
-      end;
+      end; 
    writeln(res);
 end.
