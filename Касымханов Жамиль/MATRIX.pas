b7e77fe5a34fb9e9956ed9639eb0cb12b613cc86
sahar1ne <good.ajixumuk@gmail.com>
2014-12-29 07:48:25 +0300

    #134

--- Касымханов Жамиль/MATRIX.pas
+++ Касымханов Жамиль/MATRIX.pas
@@ -1,32 +1,30 @@
-program ololo;
-
 var
 
   A: array [1..100, 1..100] of Integer;
   S, I, J, L: Integer;
 
+  
 begin
+
 randomize;
-L:=random(10); 
-   FOR I := 1 to L do
-      A[I] := random(10);
+L:=random(8)+2;
+  FOR I := 1 to L do
+     FOR J := 1 to L do
+        A[I, J] := random(10);
+  FOR I := 1 to L do
      begin
-   FOR I := 1 to L do
-       Write(A[I],' ');
-          Writeln; end;
+        FOR J := 1 to L do
+                Write(A[I, J],' ');
+                    Writeln;
+end;
+  
+  for I := 1 to L do
+    S := S + A[I, 1] + A[I, L];
+  for J := 1 to L do
+    S := S + A[1, J] + A[L, J];
+  S := S - A[1, 1] - A[1, L] - A[L, 1] - A[L ,L];
 
-      S := S + A[I, J];
-
-      S := S + A[I, J]; 
-
-    end;
-  ReadLn;
-  for I := 2 to L-1 do
-     for J := 2 to L-1 do
-      S := S - A[I, J];
-  WriteLn('Summa: ',S);
-
-  readln;
+  WriteLn('Cумма элементов по краям матрицы:',S);
 
 
 end.
