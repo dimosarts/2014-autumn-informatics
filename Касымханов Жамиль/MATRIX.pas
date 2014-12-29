b0653ce6c250627797dec9005f30c12393b50177
sahar1ne <good.ajixumuk@gmail.com>
2014-12-29 07:15:27 +0300

    #134

--- Касымханов Жамиль/MATRIX.pas
+++ Касымханов Жамиль/MATRIX.pas
@@ -6,23 +6,15 @@ var
   S, I, J, L: Integer;
 
 begin
-{randomize;
+randomize;
 L:=random(10); 
    FOR I := 1 to L do
       A[I] := random(10);
      begin
    FOR I := 1 to L do
        Write(A[I],' ');
-          Writeln; end;}
-  WriteLn('Vvedite razmernost');
-  ReadLn(L);
-  WriteLn('Vvedite matricu');
-  S := 0;
-  for I := 1 to L do
-    for J := 1 to L do
-    begin
-      Read(A[I, J]);
- 
+          Writeln; end;
+
       S := S + A[I, J];
 
       S := S + A[I, J]; 
