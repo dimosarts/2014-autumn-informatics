b9e7625bbba16b033803745271b2774a6e62dfb4
sahar1ne <good.ajixumuk@gmail.com>
2014-12-29 07:04:53 +0300

    #136

--- Касымханов Жамиль/STRING-izmennnaya.pas
+++ Касымханов Жамиль/STRING-izmennnaya.pas
@@ -2,22 +2,36 @@ uses Timers;
 
 var
   S: String;
-  I, P, x: Integer;
-
+  I, P, x, J, O, y: Integer;
+  k, m, z: real;
 begin
   WriteLn('Vvedite stroku:');
-  ReadLn(S);
+  Read(S);
 
     var start:=System.DateTime.Now;
 
   for x := 1 to 1000000000 do
-  p:=0;
+  P:=0;
   For I:= Length(S) downto 1 do
-  If S[I] = ',' then begin p:=I; break;  end;
+      If S[I] = ',' then begin P:=I; break;  end;
   WriteLn('Poziciya poslednei zapyatoi: ', P);
 
     var finish:=System.DateTime.Now;
-    writeln((finish-start).TotalMilliseconds);
+    writeln('Vremya s ciklom DOWNTO: ', (finish-start).TotalMilliseconds);
+k:=((finish-start).TotalMilliseconds);
 
-    readln;
+    var start1:=System.DateTime.Now;
+
+  for y := 1 to 1000000000 do
+  O:=0;
+  for J:=1 to Length(S) do
+      if S[J]=',' then O:= J;
+  WriteLn('Poziciya poslednei zapyatoi: ', O);
+
+    var finish1:=System.DateTime.Now;
+    writeln('Vremya s ciklom TO: ', (finish1-start1).TotalMilliseconds);
+m:=(finish1-start1).TotalMilliseconds;
+z:=abs(k-m);
+
+Writeln('Raznica vypolneniya 2-uh ciklov: ',z);
 end.
