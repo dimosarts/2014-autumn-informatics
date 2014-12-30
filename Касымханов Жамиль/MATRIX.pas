582c6854e762505943e7c5b11c65049b6e3bc4e9
sahar1ne <good.ajixumuk@gmail.com>
2014-12-31 02:16:52 +0300

    #

--- Касымханов Жамиль/MATRIX.pas
+++ Касымханов Жамиль/MATRIX.pas
@@ -6,23 +6,24 @@ var
 
 begin
   Randomize;
-  WriteLn('Введите размерность: ');
+  WriteLn('Vvedite razmernost');
   ReadLn(L);
+  S := 0;
   for I := 1 to L do
     for J := 1 to L do
+    begin
       A[I, J] := Random(10);
-  for I := 1 to L do
-    S := S + A[I, 1] + A[I, L];
-  for J := 1 to L do
-    S := S + A[1, J] + A[L, J];
-  S := S - A[1, 1] - A[1, L] - A[L, 1] - A[L ,L];
-  WriteLn('Матрица: ');
+      S := S + A[I, J];
+    end;
+  for I := 2 to L-1 do
+     for J := 2 to L-1 do
+      S := S - A[I, J];
+  WriteLn('Matrix: ');
   for I:= 1 to L do
   begin
     for J:= 1 to L do
       Write(A[I, J],'  ');
     WriteLn;
   end;
-  WriteLn('Cумма элементов по краям матрицы: ',S);
-  ReadLn;
+  WriteLn('Summa: ',S);
 end.
