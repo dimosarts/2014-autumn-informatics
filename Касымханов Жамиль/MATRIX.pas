b6f60b716848887f1c2b287aada0d127b1600c52
sahar1ne <good.ajixumuk@gmail.com>
2015-01-02 10:32:14 +0300

    #134

--- Касымханов Жамиль/MATRIX.pas
+++ Касымханов Жамиль/MATRIX.pas
@@ -1,11 +1,12 @@
 var
   A: array[1..100,1..100] of Integer;
-  I, L, J: Integer;
+  I, L, J, x: Integer;
   Summ: Integer;
 begin
   Randomize;
   WriteLn('Vvedite razmernost: ');
   ReadLn(L);
+var start:=System.DateTime.Now;
   for I:=1 to L do
     for J:=1 to L do
       A[I, J] := Random(10);
@@ -16,9 +17,12 @@ begin
       Write(A[I, J],'  ');
     WriteLn; 
   end;
+
+
   for I:=2 to L-1 do
     Summ := Summ + A[1, I] + A[L, I] + A[I, 1]  + A[I, L];
   Summ := Summ + A[1, 1]  + A[L, L] + A[1, L] + A[L, 1];
+
   WriteLn('Summa: ');
   WriteLn(Summ);
 
